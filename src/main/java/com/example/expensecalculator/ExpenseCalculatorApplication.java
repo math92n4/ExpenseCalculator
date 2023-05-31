729ae5a6cfe6b7278bdc2a4c7e48b477d70e1258
math92n4 <112948957+math92n4@users.noreply.github.com>
2023-05-31 18:11:56 +0200

    Update ExpenseCalculatorApplication.java

--- src/main/java/com/example/expensecalculator/ExpenseCalculatorApplication.java
+++ src/main/java/com/example/expensecalculator/ExpenseCalculatorApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExpenseCalculatorApplication {
 
+
+//test
     public static void main(String[] args) {
         SpringApplication.run(ExpenseCalculatorApplication.class, args);
     }
